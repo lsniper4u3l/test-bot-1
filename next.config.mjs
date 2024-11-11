02071872242c2443f815a8d6b905d1ae1fe61bbf
Phu <phuaeoof@gmail.com>
2024-11-11 15:35:52 +0700

    first commit1

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
-next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    output: 'export',
-  }
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+}
 
 export default nextConfig;
